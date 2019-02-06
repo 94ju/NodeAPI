7812a1f7344640408e490f1a2cdfea36c765d4d3
94ju <janithherath94@gmail.com>
2019-02-06 23:02:56 +0530

    single todo get

--- app.js
+++ app.js
@@ -44,3 +44,22 @@ app.post('/api/v1/todos', (req, res) => {
    todo
  })
 });
+
+//API 3rd phase 
+
+app.get('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  db.map((todo) => {
+    if (todo.id === id) {
+      return res.status(200).send({
+        success: 'true',
+        message: 'todo retrieved successfully',
+        todo,
+      });
+    } 
+});
+ return res.status(404).send({
+   success: 'false',
+   message: 'todo does not exist',
+  });
+});
